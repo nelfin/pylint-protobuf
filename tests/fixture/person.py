dee7876a65757f88d8fe1634f5aeb1db88420ceb
Andrew Haigh <hello@nelf.in>
2018-07-17 00:00:04 +1000

    Add a minimal self-contained example
    Executing person.py should produce a corresponding person_pb2.py module,
    and running pylint over person.py shows no warnings with a default
    install, and produces an error on line 23 warning about the invalid
    field.

---
+++ tests/fixture/person.py
@@ -0,0 +1,23 @@
+"""
+An example module with no warnings otherwise
+"""
+
+try:
+    from person_pb2 import Person
+except ImportError:
+    with open('person.proto', 'wb') as f:
+        f.write("""
+message Person {
+  required string name = 1;
+  required int32 id = 2;
+  optional string email = 3;
+}""")
+    import subprocess
+    subprocess.check_call(['protoc', 'person.proto', '--python_out=.'])
+    from person_pb2 import Person
+
+
+def main():
+    """A pedantic docstring."""
+    person = Person()
+    person.should_warn = 123
