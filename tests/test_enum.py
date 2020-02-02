354894630213dd95650a2bc9bc9748652fb8040e
Andrew Haigh <hello@nelf.in>
2020-02-02 17:32:46 +1100

    Rewrite enum tests to use new helper utilities

--- tests/test_enum.py
+++ tests/test_enum.py
@@ -1,199 +1,181 @@
 import sys
 
 import pytest
-import astroid
-import pylint.testutils
 
 import pylint_protobuf
+from conftest import CheckerTestCase, extract_node, make_message
 
 
-class TestEnumDefinitions(pylint.testutils.CheckerTestCase):
+@pytest.fixture
+def enum_mod(proto_builder):
+    return proto_builder("""
+        syntax = "proto2";
+        package test;
+        enum Variable {
+          CONTINUOUS = 0;
+          DISCRETE = 1;
+        }
+    """, 'enum')
+
+
+@pytest.fixture
+def nested_enum_mod(proto_builder):
+    return proto_builder("""
+        syntax = "proto2";
+        package test;
+
+        enum Outer {
+          UNDEFINED = 0;
+          ONE = 1;
+          TWO = 2;
+        }
+
+        message Message {
+          enum Inner {
+            UNDEFINED = 0;
+            UNO = 1;
+            DOS = 2;
+          }
+        }
+    """, 'nested_enum')
+
+
+class TestEnumDefinitions(CheckerTestCase):
     CHECKER_CLASS = pylint_protobuf.ProtobufDescriptorChecker
 
-    def test_import_enum_types_no_errors(self):
-        node = astroid.extract_node("""
-        from fixture.enum_pb2 import Variable
-        print(Variable)
-        """)
-        with self.assertNoMessages():
-            self.walk(node.root())
+    def test_import_enum_types_no_errors(self, enum_mod):
+        self.assert_no_messages(extract_node("""
+            from {} import Variable
+            print(Variable)
+        """.format(enum_mod)))
 
-    def test_import_enum_values_no_errors(self):
-        node = astroid.extract_node("""
-        from fixture.enum_pb2 import DISCRETE
-        print(DISCRETE)
-        """)
-        with self.assertNoMessages():
-            self.walk(node.root())
+    def test_import_enum_values_no_errors(self, enum_mod):
+        self.assert_no_messages(extract_node("""
+            from {} import DISCRETE
+            print(DISCRETE)
+        """.format(enum_mod)))
 
-    def test_import_enum_attributes_no_errors(self):
-        node = astroid.extract_node("""
-        from fixture.enum_pb2 import Variable
-        print(Variable.DISCRETE)
-        """)
-        with self.assertNoMessages():
-            self.walk(node.root())
+    def test_import_enum_attributes_no_errors(self, enum_mod):
+        self.assert_no_messages(extract_node("""
+            from {} import Variable
+            print(Variable.DISCRETE)
+        """.format(enum_mod)))
 
-    def test_import_enum_missing_attributes_warns(self):
-        node = astroid.extract_node("""
-        from fixture.enum_pb2 import Variable
-        print(
+    def test_import_enum_missing_attributes_warns(self, enum_mod):
+        node = extract_node("""
+            from {} import Variable
+            print(
+                Variable.should_warn  #@
+            )
+        """.format(enum_mod))
+        msg = make_message(node, enum_mod+'.Variable', 'should_warn')
+        self.assert_adds_messages(node, msg)
+
+    def test_import_enum_by_value_no_errors(self, enum_mod):
+        self.assert_no_messages(extract_node("""
+            from {} import Variable
+            print(Variable.Value('DISCRETE'))
+        """.format(enum_mod)))
+
+    def test_star_import_enum_no_errors(self, enum_mod):
+        self.assert_no_messages(extract_node("""
+            from {} import *
+            print(DISCRETE)
+        """.format(enum_mod)))
+
+    def test_star_import_enum_should_warn(self, enum_mod):
+        node = extract_node("""
+            from {} import *
             Variable.should_warn  #@
-        )
-        """)
-        message = pylint.testutils.Message(
-            'protobuf-undefined-attribute',
-            node=node,
-            args=('should_warn', 'fixture.enum_pb2.Variable')
-        )
-        with self.assertAddsMessages(message):
-            self.walk(node.root())
-
-    def test_import_enum_by_value_no_errors(self):
-        node = astroid.extract_node("""
-        from fixture.enum_pb2 import Variable
-        print(Variable.Value('DISCRETE'))
-        """)
-        with self.assertNoMessages():
-            self.walk(node.root())
-
-    def test_star_import_enum_no_errors(self):
-        node = astroid.extract_node("""
-        from fixture.enum_pb2 import *
-        print(DISCRETE)
-        """)
-        with self.assertNoMessages():
-            self.walk(node.root())
-
-    def test_star_import_enum_should_warn(self):
-        node = astroid.extract_node("""
-        from fixture.enum_pb2 import *
-        Variable.should_warn  #@
-        """)
-        message = pylint.testutils.Message(
-            'protobuf-undefined-attribute',
-            node=node,
-            args=('should_warn', 'fixture.enum_pb2.Variable')
-        )
-        with self.assertAddsMessages(message):
-            self.walk(node.root())
+        """.format(enum_mod))
+        msg = make_message(node, enum_mod+'.Variable', 'should_warn')
+        self.assert_adds_messages(node, msg)
 
     @pytest.mark.xfail(reason='unimplemented')
-    def test_import_enum_missing_attribute_by_value_warns(self):
-        node = astroid.extract_node("""
-        from fixture.enum_pb2 import Variable
+    def test_import_enum_missing_attribute_by_value_warns(self, enum_mod):
+        node = extract_node("""
+        from {} import Variable
         print(
             Variable.Value('should_warn')  #@
         )
-        """)
-        message = pylint.testutils.Message(
-            'protobuf-enum-value',
-            node=node,
-            args=('should_warn', 'fixture.enum_pb2.Variable')
-        )
-        with self.assertAddsMessages(message):
-            self.walk(node.root())
+        """.format(enum_mod))
+        msg = make_message(node, enum_mod+'.Variable', 'should_warn')
+        self.assert_adds_messages(node, msg)
 
-    def test_issue16_toplevel_enum(self):
-        node = astroid.extract_node("""
-        import fixture.nested_enum_pb2
-        fixture.nested_enum_pb2.ONE
-        """)
-        with self.assertNoMessages():
-            self.walk(node.root())
+    def test_issue16_toplevel_enum(self, nested_enum_mod):
+        self.assert_no_messages(extract_node("""
+            import {mod}
+            {mod}.ONE
+        """.format(mod=nested_enum_mod)))
 
-    def test_issue16_nested_enum_definition_no_errors(self):
-        node = astroid.extract_node("""
-        import fixture.nested_enum_pb2
-        fixture.nested_enum_pb2.Message.UNO
-        """)
-        with self.assertNoMessages():
-            self.walk(node.root())
+    def test_issue16_nested_enum_definition_direct_reference_no_errors(self, nested_enum_mod):
+        self.assert_no_messages(extract_node("""
+            import {mod} as sut
+            {mod}.Message.Inner.UNO
+        """.format(mod=nested_enum_mod)))
 
     @pytest.mark.xfail(reason='nested namespaces unimplemented')
-    def test_fixme_issue16_nested_enum_definition_no_errors(self):
-        node = astroid.extract_node("""
-        import fixture.nested_enum_pb2 as sut
-        sut.Message.UNO
-        """)
-        with self.assertNoMessages():
-            self.walk(node.root())
+    def test_issue16_nested_enum_definition_no_errors(self, nested_enum_mod):
+        self.assert_no_messages(extract_node("""
+            import {mod}
+            {mod}.Message.UNO
+        """.format(mod=nested_enum_mod)))
 
     @pytest.mark.xfail(reason='nested namespaces unimplemented')
-    def test_issue16_package_nested_enum_definition_warns(self):
-        node = astroid.extract_node("""
-        import fixture.nested_enum_pb2
-        fixture.nested_enum_pb2.Message.should_warn
-        """)
-        message = pylint.testutils.Message(
-            'protobuf-undefined-attribute',
-            node=node,
-            args=('should_warn', 'fixture.nested_enum_pb2.Message')
-        )
-        with self.assertAddsMessages(message):
-            self.walk(node.root())
-
-    def test_issue16_nested_enum_definition_warns(self):
-        node = astroid.extract_node("""
-        import fixture.nested_enum_pb2 as sut
-        sut.Message.should_warn
-        """)
-        message = pylint.testutils.Message(
-            'protobuf-undefined-attribute',
-            node=node,
-            args=('should_warn', 'fixture.nested_enum_pb2.Message')
-        )
-        with self.assertAddsMessages(message):
-            self.walk(node.root())
-
-    def test_issue16_missing_toplevel_enum(self):
-        node = astroid.extract_node("""
-        import fixture.nested_enum_pb2 as sut
-        sut.UNO
-        """)
-        message = pylint.testutils.Message(
-            'protobuf-undefined-attribute',
-            node=node,
-            args=('UNO', 'fixture.nested_enum_pb2')
-        )
-        with self.assertAddsMessages(message):
-            self.walk(node.root())
+    def test_fixme_issue16_nested_enum_definition_no_errors(self, nested_enum_mod):
+        self.assert_no_messages(extract_node("""
+            import {} as sut
+            sut.Message.UNO
+        """.format(nested_enum_mod)))
 
     @pytest.mark.xfail(reason='nested namespaces unimplemented')
-    def test_issue16_package_missing_toplevel_enum(self):
-        node = astroid.extract_node("""
-        import fixture.nested_enum_pb2
-        fixture.nested_enum_pb2.UNO
-        """)
-        message = pylint.testutils.Message(
-            'protobuf-undefined-attribute',
-            node=node,
-            args=('UNO', 'fixture.nested_enum_pb2')
-        )
-        with self.assertAddsMessages(message):
-            self.walk(node.root())
+    def test_issue16_package_nested_enum_definition_warns(self, nested_enum_mod):
+        node = extract_node("""
+            import {mod}
+            {mod}.Message.should_warn
+        """.format(mod=nested_enum_mod))
+        msg = make_message(node, nested_enum_mod+'.Message', 'should_warn')
+        self.assert_adds_messages(node, msg)
+
+    def test_issue16_nested_enum_definition_warns(self, nested_enum_mod):
+        node = extract_node("""
+            import {} as sut
+            sut.Message.should_warn
+        """.format(nested_enum_mod))
+        msg = make_message(node, nested_enum_mod+'.Message', 'should_warn')
+        self.assert_adds_messages(node, msg)
+
+    def test_issue16_missing_toplevel_enum(self, nested_enum_mod):
+        node = extract_node("""
+            import {} as sut
+            sut.UNO
+        """.format(nested_enum_mod))
+        msg = make_message(node, nested_enum_mod, 'UNO')
+        self.assert_adds_messages(node, msg)
+
+    @pytest.mark.xfail(reason='nested namespaces unimplemented')
+    def test_issue16_package_missing_toplevel_enum(self, nested_enum_mod):
+        node = extract_node("""
+            import {mod}
+            {mod}.UNO
+        """.format(mod=nested_enum_mod))
+        msg = make_message(node, nested_enum_mod, 'UNO')
+        self.assert_adds_messages(node, msg)
 
     @pytest.mark.skipif(sys.version_info < (3, 0),
                         reason='function annotations are Python 3+')
-    def test_issue21_nested_enum_annassign(self):
-        node = astroid.extract_node("""
-        import fixture.nested_enum_pb2 as sut
-        def fun(type_: sut.Message.Inner):
-            pass
-        """)
-        with self.assertNoMessages():
-            self.walk(node.root())
+    def test_issue21_nested_enum_annassign(self, nested_enum_mod):
+        self.assert_no_messages(extract_node("""
+            import {} as sut
+            def fun(type_: sut.Message.Inner):
+                pass
+        """.format(nested_enum_mod)))
 
     @pytest.mark.xfail(reason='issue #21 unfixed')
-    def test_issue21_missing_field_on_nested_enum(self):
-        node = astroid.extract_node("""
-        import fixture.nested_enum_pb2 as sut
-        sut.Message.Inner.NOPE = 123
-        """)
-        message = pylint.testutils.Message(
-            'protobuf-undefined-attribute',
-            node=node.targets[0],
-            args=('NOPE', 'fixture.nested_enum_pb2.Message.Inner')
-        )
-        with self.assertAddsMessages(message):
-            self.walk(node.root())
+    def test_issue21_missing_field_on_nested_enum(self, nested_enum_mod):
+        node = extract_node("""
+            import {} as sut
+            sut.Message.Inner.NOPE = 123
+        """.format(nested_enum_mod))
+        msg = make_message(node.targets[0], nested_enum_mod+'.Message.Inner', 'NOPE')
+        self.assert_adds_messages(node, msg)
