1c4caaa926ced28a9419bc2d06e1e93386653eb7
Andrew Haigh <hello@nelf.in>
2021-02-12 22:13:59 +1100

    Add XFAIL test of positional arguments check
    Manual testing shows that generated protobuf message constructors only
    accept kwargs (at least as of 3.14). In the future we should warn about
    this behaviour as it will raise TypeError at runtime.

--- tests/test_issue30.py
+++ tests/test_issue30.py
@@ -50,3 +50,26 @@ def test_E1123_on_unexpected_kwargs(warnings_mod, linter_factory):
     ]
     actual_messages = [m.msg for m in linter.reporter.messages]
     assert sorted(actual_messages) == sorted(expected_messages)
+
+
+@pytest.fixture
+def posargs_mod(kwarg_mod, module_builder):
+    return module_builder("""
+        from {} import Count
+        c = Count(0)
+    """.format(kwarg_mod), 'posargs_mod')
+
+
+@pytest.mark.xfail(reason='unimplemented')
+def test_warn_typeerror_on_positional_args(posargs_mod, linter_factory):
+    linter = linter_factory(
+        register=pylint_protobuf.register,
+        disable=['all'], enable=['protobuf-type-error'],  # TODO
+    )
+    linter.check([posargs_mod])
+    expected_messages = [
+        'Positional arguments are not allowed in message constructors and will raise TypeError'
+    ]
+    actual_messages = [m.msg for m in linter.reporter.messages]
+    assert actual_messages  # to make this XPASS
+    assert sorted(actual_messages) == sorted(expected_messages)
